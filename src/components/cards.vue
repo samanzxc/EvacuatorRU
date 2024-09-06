2ddac176b11ff40fd611497cec850cb17145d563
samanzxc <samanmuh@yandex.ru>
2024-09-06 17:30:41 +0300

    d

--- src/components/cards.vue
+++ src/components/cards.vue
@@ -10,7 +10,7 @@ import cardInfo from './cardInfo.json'
         <div class="card__title">{{ card.cardName }}</div>
         <div class="card__info">
           <div class="card__img">
-            <img :src="`/src/assets/img/evacuator${index+1}.png`" alt="imgAuto" />
+            <img :src="`/EvacuatorRU/assets/${card.cardImg}`" alt="imgAuto" />
           </div>
           <div class="card__price">
             <div class="card__price__tab">
